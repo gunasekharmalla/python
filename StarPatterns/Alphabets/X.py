0277287700111b04f17d3058c579d664d2cad443
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-26 16:15:42 +0530

    Update X.py

--- StarPatterns/Alphabets/X.py
+++ StarPatterns/Alphabets/X.py
@@ -1,1 +1,19 @@
 
+n = 5
+
+for i in range(n,0,-1):
+    print(" "*(n-i)+"*"*(i) if i==1 else " "*(n-i)+"*"+" "*((2*i)-3)+"*")
+for i in range(2,n+1):
+    print(" "*(n-i)+"*"*(i) if i==1 else " "*(n-i)+"*"+" "*((2*i)-3)+"*")
+
+output:
+
+*       *
+ *     *
+  *   *
+   * *
+    *
+   * *
+  *   *
+ *     *
+*       *
