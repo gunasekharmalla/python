47645a1d51c8e3fd3c5841e951cc73c6f0309a6f
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-26 11:21:17 +0530

    Update B.py

--- StarPatterns/Alphabets/B.py
+++ StarPatterns/Alphabets/B.py
@@ -1,1 +1,18 @@
 
+#letter B 
+
+n = 7
+c = n//2 + 1
+for i in range(1,n+1):
+        print("* "*(c-1) +" " if i == 1 or i == n or i == c else "* "+" "*(c)+"*")
+
+
+output:
+
+* * *  
+*     *
+*     *
+* * *  
+*     *
+*     *
+* * *  
