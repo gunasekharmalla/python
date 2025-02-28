e875cce8f107f332758417623eafb0ea98201f81
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-28 13:45:13 +0530

    Update HollowDiamond.py

--- StarPatterns/HollowDiamond.py
+++ StarPatterns/HollowDiamond.py
@@ -1,1 +1,24 @@
+# Hollow Diamond pattern for n rows 
+n=5
+for I in range(1,n+1):
+    print(" "*(n-I)+"*" if I==1 else(" "*(n-I)+"*"+" "*((2*I)-3)+"*"))
+for I in range(n-1,0,-1):
+    print(" "*(n-I)+"*" if I==1 else(" "*(n-I)+"*"+" "*((2*I)-3)+"*"))
 
+Output:
+
+    *
+
+   * *
+
+  *   *
+
+ *     *
+
+*       *
+
+ *     *
+
+  *   *
+
+    *
