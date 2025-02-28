a8926cc1e8a77b1c2f586b70eecff8e8e60cad68
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-28 11:13:09 +0530

    Update Rectangle.py

--- StarPatterns/Rectangle.py
+++ StarPatterns/Rectangle.py
@@ -1,1 +1,22 @@
+#Rectangle pattern 
 
+n=10
+c =  n//2 + 1
+print("* "*n)
+for I in range(n-c):
+    print("* "+"  " *(n-2) +"*")
+print("* "*n
+
+output:
+
+* * * * * * * * * * 
+
+*                 *
+
+*                 *
+
+*                 *
+
+*                 *
+
+* * * * * * * * * *
