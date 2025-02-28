506371a7f25e816b2b84e43db01a0f3e230080cf
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-28 11:46:04 +0530

    Update HollowTriangle.py

--- StarPatterns/HollowTriangle.py
+++ StarPatterns/HollowTriangle.py
@@ -1,1 +1,17 @@
+#hollow Triangle
+n=5
+for I in range(1,n+1):
+    print("*"*1 if I==1 else(" "*(n-I)+"*"+" " *((2*I)-3) +"*"))
+    if I==n:
+        print("*"*(((2*I)-3)+2
 
+                   
+output:
+
+      
+    *
+   * *
+  *   *
+ *     *
+*       *
+*********
