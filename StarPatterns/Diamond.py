fda9f48723d4f9903657dbd7446f0dba7372a6d2
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-02-28 13:55:32 +0530

    Update Diamond.py

--- StarPatterns/Diamond.py
+++ StarPatterns/Diamond.py
@@ -1,1 +1,29 @@
+# diamond pattern 
+
+n=5
+for I in range(1,n+1):
+    print(" "*(n-I)+"*" if I==1 else(" "*(n-I)+"*"*((2*I)-1)))
+for I in range(n-1,0,-1):
+    print(" "*(n-I)+"*" if I==1 else(" "*(n-I)+"*"*((2*I)-1)))
+
+Output:
+
+
+    *
+
+   ***
+
+  *****
+
+ *******
+
+*********
+
+ *******
+
+  *****
+
+   ***
+
+    *
 
