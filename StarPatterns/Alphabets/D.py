45fdfe2f40b7dc170634bc8748349a4230a664e9
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-03-01 08:37:42 +0530

    Update D.py

--- StarPatterns/Alphabets/D.py
+++ StarPatterns/Alphabets/D.py
@@ -1,1 +1,24 @@
+#letter D pattern 
 
+
+n =7
+print("* "*(n))
+for I in range(2,n):
+    print("*"+"   "*(n-2)+"*")
+print("* "*(n))
+
+Output: 
+
+* * * * * * * 
+
+*               *
+
+*               *
+
+*               *
+
+*               *
+
+*               *
+
+* * * * * * * 
