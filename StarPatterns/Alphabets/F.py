7bd251949fbdd8f01e46ea46c7b8d81fa82655d6
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-03-01 08:58:26 +0530

    Update F.py

--- StarPatterns/Alphabets/F.py
+++ StarPatterns/Alphabets/F.py
@@ -1,1 +1,23 @@
+#pattern F 
 
+n =7
+c = n//2 + 1
+print("* "*(n))
+for I in range(2,n+1):
+    print("* "*(n) if I==c else("*"))
+
+Output:
+
+* * * * * * * 
+
+*
+
+*
+
+* * * * * * * 
+
+*
+
+*
+
+*
