9bddcbbdbe55620f8a840ca43a61170c2ae8a68f
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-03-01 08:53:48 +0530

    Update E.py

--- StarPatterns/Alphabets/E.py
+++ StarPatterns/Alphabets/E.py
@@ -1,1 +1,24 @@
+#letter E 
 
+n =7
+c = n//2 + 1
+print("* "*(n))
+for I in range(2,n):
+    print("* "*(n) if I==c else("*"))
+print("* "*(n))
+
+Output: 
+
+* * * * * * * 
+
+*
+
+*
+
+* * * * * * * 
+
+*
+
+*
+
+* * * * * * *
