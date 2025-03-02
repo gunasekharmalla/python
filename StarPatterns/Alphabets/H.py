4dc474e185a992f81c5e408319261c62c66b873b
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-03-02 17:48:07 +0530

    Update H.py

--- StarPatterns/Alphabets/H.py
+++ StarPatterns/Alphabets/H.py
@@ -1,1 +1,22 @@
+# H pattern 
 
+n= 7
+c = n//2 + 1 
+for I in range(1,n+1):
+    print("* "+"  "*(n-2)+"*" if I!=c else("* "*(n)))
+
+Output: 
+
+*           *
+
+*           *
+
+*           *
+
+* * * * * * * 
+
+*           *
+
+*           *
+
+*           *  
