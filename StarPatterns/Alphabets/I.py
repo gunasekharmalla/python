a6c3e5791d016ec21221625289b6dc5926d1b2d1
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-03-02 17:59:30 +0530

    Update I.py

--- StarPatterns/Alphabets/I.py
+++ StarPatterns/Alphabets/I.py
@@ -1,1 +1,30 @@
+# I pattern 
 
+
+n= 7
+c = n//2 +1
+print("* "*(n))
+for I in range(2,n):
+    print("  "*(c-1)+"*")
+print("* "*(n))
+
+
+Output: 
+
+* * * * * * * 
+
+      *
+
+      *
+
+      *
+
+      *
+
+      *
+
+* * * * * * * 
+
+
+
+=== Code Exec
