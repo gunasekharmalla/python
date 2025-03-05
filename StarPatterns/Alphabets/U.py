32bea78323e6ce9c03eb09459ff6b604c50f6e81
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-03-05 18:38:24 +0530

    Update U.py

--- StarPatterns/Alphabets/U.py
+++ StarPatterns/Alphabets/U.py
@@ -1,1 +1,21 @@
+# U pattern 
 
+n=7
+for I in range(1,n+1):
+    print("* "*(n)if I==n else("*"+"  "*(n-2)+" *"))
+
+Output:
+
+*           *
+
+*           *
+
+*           *
+
+*           *
+
+*           *
+
+*           *
+
+* * * * * * *
