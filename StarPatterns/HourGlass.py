cea323349d89ae746c1c994053b465bfd9547911
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-03-11 22:51:01 +0530

    Update HourGlass.py

--- StarPatterns/HourGlass.py
+++ StarPatterns/HourGlass.py
@@ -1,1 +1,19 @@
+n = 5
+for i in range(n, 0, -1):
+    print(" " * (n-i) + "* " * i)
+for i in range(2, n+1):
+    print(" " * (n-i) + "* " * i)
+
+output:
+
+* * * * * 
+ * * * *  
+  * * *   
+   * *    
+    *     
+    *     
+   * *    
+  * * *   
+ * * * *  
+* * * * * 
 
