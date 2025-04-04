5c878a2520187ea024ffe104920a1a4b178a8b1c
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-04 22:27:08 +0530

    Update Guessing.py

--- basic-projects/NumGuessing/Guessing.py
+++ basic-projects/NumGuessing/Guessing.py
@@ -1,1 +1,28 @@
+import random
+
+guess = random.randrange(100)
+
+chances = 7
+
+count = 0
+
+print("NUMBER guessing game")
+print("=============================")
+while count < chances:
+
+    count += 1
+    my_guess = int(input('Please Enter your Guess : '))
+
+    if my_guess == guess:
+        print(f'The number is {number_to_guess} and you found it right !! in the {count} attempt')
+        break
+
+    elif count >= chances and my_guess != guess:
+        print(f'Oops sorry, The number is {guess} better luck next time')
+
+    elif my_guess > guess:
+        print('Your guess is higher ')
+
+    elif my_guess < guess:
+        print('Your guess is lesser')
 
