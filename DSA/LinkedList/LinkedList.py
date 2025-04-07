52cd75f1455a2911852367237725a13075f83258
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-07 23:27:01 +0530

    Update LinkedList.py

--- DSA/LinkedList/LinkedList.py
+++ DSA/LinkedList/LinkedList.py
@@ -1,1 +1,89 @@
+What is a Linked List?
+============================
+  => Linear data structure where elements (nodes) point to the next.
+     Dynamic memory allocation (no fixed size like arrays).
 
+Types of Linked Lists:
+========================
+-> Singly Linked List
+-> Doubly Linked List
+-> Circular Linked List (Singly and Doubly)
+
+Basic Terms:
+========================
+
+-> Node
+-> Head
+-> Tail
+-> Null/None reference
+
+Stage 2: Singly Linked List
+===================================
+
+Creating a Node class
+-> Inserting:
+-> At the beginning
+-> At the end
+-> At a given position
+
+Deleting:
+-> From beginning
+-> From end
+-> Specific node
+
+Traversal (printing the list)
+-> Searching an element
+
+Stage 3: Doubly Linked List
+=====================================
+=> Node structure (prev, data, next)
+=> Insertion and deletion (all positions)
+=> Reversing a doubly linked list
+
+Stage 4: Circular Linked List
+===========================================
+-> Circular Singly vs Circular Doubly
+-> Traversal (careful to avoid infinite loops)
+-> Insert/Delete in circular list
+
+
+Stage 5: Practice Problems
+===========================================
+Beginner:
+Find length of linked list
+
+Search an element
+
+Reverse a linked list
+
+Detect loop (Floyd’s Algorithm)
+
+Intermediate:
+===================================
+
+Remove duplicates
+
+Merge two sorted linked lists
+
+Middle of the linked list
+
+Intersection point in two linked lists
+
+Advanced:
+==============================================
+
+LRU Cache (uses doubly linked list + hashmap)
+
+Copy list with random pointer
+
+Flatten a multilevel linked list
+
+Sort a linked list (Merge Sort)
+
+Stage 6: Master Tips
+===================================================
+Use recursion to reverse or print backward
+
+Dry run pointer changes (especially in deletion)
+
+Understand memory leaks and garbage collection (especially in python/C/C++)
