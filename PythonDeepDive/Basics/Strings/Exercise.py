95adad5c02aa86679013a23a0592a8bc76586838
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-09 21:51:13 +0530

    Update Exercise.py

--- PythonDeepDive/Basics/Strings/Exercise.py
+++ PythonDeepDive/Basics/Strings/Exercise.py
@@ -1,1 +1,40 @@
+✅ Task 1: Count Vowels
+
+Input: "Hello World"
+Output: 3 (e, o, o)
+# Write a function to count vowels (a, e, i, o, u)
+def count_vowels(s):
+    pass  
+  =============================================
+✅ Task 2: Convert Sentence to Title Case
+Input: "python is fun"
+Output: "Python Is Fun"
+# Write a function to title-case a sentence
+def to_title_case(sentence):
+    pass 
+===================================================
+✅ Task 3: Replace Spaces with Hyphens
+Input: "python is awesome"
+Output: "python-is-awesome"
+# Replace all spaces with hyphens
+def space_to_hyphen(s):
+    pass  
+====================================================
+✅ Task 4: Remove All Punctuation
+Input: "Hello!!!, my name is..."
+Output: "Hello my name is"
+Hint: Use the string module: import string
+import string
+# Remove all punctuation from the string
+def remove_punctuations(s):
+    pass 
+==============================================
+✅ Task 5: Check for Palindrome
+Input: "madam"
+Output: True
+Input: "hello"
+Output: False
+# Check if the string is a palindrome
+def is_palindrome(s):
+    pass 
 
