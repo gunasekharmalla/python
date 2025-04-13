b4ed2401f30485292394973bd3c91ae1b93012bf
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-13 21:24:49 +0530

    Update Practice_qsn.py

--- DSA/Arrays/Practice_qsn.py
+++ DSA/Arrays/Practice_qsn.py
@@ -1,1 +1,58 @@
+                                        ✅ DSA Arrays Progress
+Basic Level:
+================================================
+
+       Segregate 0s and 1s
+      
+       Sort Colors (Dutch National Flag Problem)
+      
+       Rotate Array by K Places
+      
+       Find First Missing Positive Number
+
+Next Topics to Cover:
+================================================
+
+        Find the Largest and Smallest Element in an Array
+        
+        Second Largest and Second Smallest Element
+        
+        Check if an Array is Sorted
+        
+        Remove Duplicates from a Sorted Array
+        
+        Move All Negative Numbers to One Side
+        
+        Union and Intersection of Two Sorted Arrays
+
+Intermediate Level:
+================================================
+
+          Kadane’s Algorithm (Maximum Subarray Sum)
+          
+          Majority Element in an Array
+          
+          Rearrange Array Alternating Positive and Negative Numbers
+          
+          Next Permutation
+          
+          Stock Buy and Sell Problem
+          
+          Subarray with Given Sum
+
+Advanced Level:
+==================================================
+
+          Longest Consecutive Subsequence
+          
+          Count Inversions in an Array
+          
+          Trapping Rain Water
+          
+          Maximum Product Subarray
+          
+          Median of Two Sorted Arrays
+          
+          Minimum Swaps to Sort
+
 
