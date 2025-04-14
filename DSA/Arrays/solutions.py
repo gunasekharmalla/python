4a89c0574e29514f79408953f0938e12758509c4
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-14 21:53:25 +0530

    Update solutions.py

--- DSA/Arrays/solutions.py
+++ DSA/Arrays/solutions.py
@@ -1,1 +1,15 @@
-
+Problem 1: 
+    Find the Largest and Smallest Element in an Array
+    Problem Statement:
+    Given an array of integers, find the smallest and largest elements.
+    Example:
+    Input: [3, 5, 1, 2, 4, 8]
+    Output: Smallest: 1, Largest: 8
+    #program:
+    maxi, mini = float('-inf'),float('inf')
+    for i in nums:
+        if i > maxi:
+            maxi = i
+        if i < mini:
+            mini = i 
+    print(maxi,mini)
