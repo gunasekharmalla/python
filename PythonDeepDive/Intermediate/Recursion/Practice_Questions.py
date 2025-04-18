e1751d7853929c9bdc0b6d229fe19653667d1a89
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-18 16:35:17 +0530

    Update Practice_Questions.py

--- PythonDeepDive/Intermediate/Recursion/Practice_Questions.py
+++ PythonDeepDive/Intermediate/Recursion/Practice_Questions.py
@@ -1,1 +1,12 @@
+                                        Basic Recursion Practice Problems
+                                      ======================================
+
+1. Factorial of a Number
+2. Fibonacci Series (Recursive)
+3. Sum of First N Natural Numbers
+4. Reverse a String Using Recursion
+5. Checking if a Number is Palindrome
+6. Binary Search Using Recursion
+
+
 
