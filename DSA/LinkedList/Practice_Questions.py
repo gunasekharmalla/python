f0137bf4e86ebe285010baa33bd3e9a291d79d20
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-18 15:54:35 +0530

    Update Practice_Questions.py

--- DSA/LinkedList/Practice_Questions.py
+++ DSA/LinkedList/Practice_Questions.py
@@ -1,1 +1,25 @@
+                                          Basic Questions from Beginner to Intermediate
+                                      ======================================================
+Basic:
+===========
+
+1. Basic Node Creation
+2. Printing the Linked List
+3. Insertion in a Singly Linked List
+      There are three common types of insertion:
+        =>Insert at the beginning
+        =>Insert at the end
+        =>Insert at a specific position/index
+4. Deleting Nodes in a Singly Linked List
+      We'll cover 3 common cases:
+        =>Delete from the beginning
+        =>Delete from the end
+        =>Delete from a specific position
+5. Reverse a Linked List
+ Problem:
+    Reverse the direction of a singly linked list.
+    Example:
+    10 → 20 → 30 → None
+    becomes
+    30 → 20 → 10 → None
 
