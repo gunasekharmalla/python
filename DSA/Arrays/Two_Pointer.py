49f12fab6cc96a59c9887b33a12348c49be8deb2
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-21 23:46:29 +0530

    Update Two_Pointer.py

--- DSA/Arrays/Two_Pointer.py
+++ DSA/Arrays/Two_Pointer.py
@@ -1,1 +1,12 @@
-
+    ## Move All Negative Numbers to One Side
+Input: [-1, 2, -3, 4, -5, 6]
+Output: [-1, -3, -5, 4, 2, 6]
+#program:
+  nums=[-1,2,-3,4,-5,6]
+  l=0
+  r=0
+  for r in range(len(nums)):
+      if nums[r]<0:
+          nums[r],nums[l]=nums[l],nums[r]
+          l+=1
+  print(nums)
