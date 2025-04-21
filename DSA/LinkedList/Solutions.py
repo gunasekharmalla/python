e77c18b627c6fdcbf0fd6b4cc6ee5ab8fbd2dfa6
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-21 22:32:13 +0530

    Update Solutions.py

--- DSA/LinkedList/Solutions.py
+++ DSA/LinkedList/Solutions.py
@@ -1,1 +1,17 @@
-
+        Delete Node based on Position
+==================================================
+def deleteNode(llist, position):
+    # Write your code here
+    if not llist:
+        return None 
+    if position == 0:
+        return llist.next 
+    count = 0
+    current = llist
+    while current and count < position -1:
+        current = current.next 
+        count += 1
+    if current is None or current.next is None:
+        return llist 
+    current.next = current.next.next 
+    return llist
