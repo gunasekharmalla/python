61fa655a908ab46ecbcec9d3dedc26a71020d142
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-22 21:59:20 +0530

    Update Solutions.py

--- DSA/LinkedList/Solutions.py
+++ DSA/LinkedList/Solutions.py
@@ -15,3 +15,26 @@ def deleteNode(llist, position):
         return llist 
     current.next = current.next.next 
     return llist
+====================================
+problem : Finding Intersection of two Lists 
+List 1: 10 -> 20 -> 30 -> 40 -> 50
+List 2:      15 -> 30 -> 40 -> 50
+
+def get_intersection(head1, head2):
+    if not head1 or not head2:
+        return None
+    
+    a, b = head1, head2
+    
+    while a != b:
+        a = a.next if a else head2
+        b = b.next if b else head1
+        
+    if a:
+        print(f"✅ Intersection point at node with value: {a.data}")
+    else:
+        print("❌ No intersection found")
+    return a
+
+Output: 
+✅ Intersection point at node with value: 30
