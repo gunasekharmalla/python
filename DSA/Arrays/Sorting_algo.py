80baeed86b5ac427aaa9b41243d7cd66a3958d66
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-22 22:04:44 +0530

    Update Sorting_algo.py

--- DSA/Arrays/Sorting_algo.py
+++ DSA/Arrays/Sorting_algo.py
@@ -99,3 +99,23 @@ lst = Merge_sorting(lst)
 print(f"sorted list : {lst}")
 
 sorted list: [1, 2, 3, 5, 6, 8, 9]
+===========================
+# insertion sort 
+arr = [12, 11,  13,  5,  6]
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]  # Current element to be inserted
+        j = i - 1
+        # Shift elements to the right to create space for key
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+arr = list(map(int, input("Enter numbers separated by space: ").split()))
+insertion_sort(arr)
+print("Sorted Array:", arr)
+
+Output : 
+Sorted Array: [5, 6, 11, 12, 13]
