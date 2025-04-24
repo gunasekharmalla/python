233a88496750def55f3de0f173445c78f8564875
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-24 22:21:29 +0530

    Update Solutions.py

--- PythonDeepDive/Basics/Lists/Solutions.py
+++ PythonDeepDive/Basics/Lists/Solutions.py
@@ -1,1 +1,14 @@
+                        Beginner level
+              ====================================
 
+ls = ["Thandel", "Legend", "Maharshi", "Mirchi", "GunturKaaram"]
+
+ls.insert(2, "HighHeat")         # Inserts "HighHeat" at index 2
+ls.append("PellisandaD")         # Adds "PellisandaD" to the end
+ls.remove("Mirchi")              # Removes "Mirchi" from the list
+
+print(f"elements in list : {ls}, len of list is : {len(ls)} ")
+print(f"elements in list : {[items for items in ls]}, len of list is : {len(ls)} ")
+Output:
+elements in list : ['Thandel', 'Legend', 'HighHeat', 'Maharshi', 'GunturKaaram', 'PellisandaD'], len of list is : 6
+elements in list : ['Thandel', 'Legend', 'HighHeat', 'Maharshi', 'GunturKaaram', 'PellisandaD'], len of list is : 6
