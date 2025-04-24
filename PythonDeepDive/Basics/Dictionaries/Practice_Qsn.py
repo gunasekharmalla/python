4e69b3e0ac3f9e64645d154e65441101fcf75f61
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-24 22:26:32 +0530

    Update Practice_Qsn.py

--- PythonDeepDive/Basics/Dictionaries/Practice_Qsn.py
+++ PythonDeepDive/Basics/Dictionaries/Practice_Qsn.py
@@ -1,1 +1,14 @@
+Tasks from basic
+=====================================
+Task 1: Create a Dictionary
 
+Create a dictionary car with the following key-value pairs:
+brand: "Tesla"
+model: "Model 3"
+year: 2022
+
+Then:
+Print the model
+Add a key color with value "red"
+Update year to 2023
+Delete the brand key
