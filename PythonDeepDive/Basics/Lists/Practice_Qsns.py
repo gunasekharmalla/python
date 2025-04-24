d558dfd713496a29cfb4845e46dbeb4b8a7df8a7
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-04-24 22:19:26 +0530

    Update Practice_Qsns.py

--- PythonDeepDive/Basics/Lists/Practice_Qsns.py
+++ PythonDeepDive/Basics/Lists/Practice_Qsns.py
@@ -1,1 +1,7 @@
-
+TASKS for Beginner Level
+=========================================
+   * Create a list of your 5 favorite movies.
+   * Replace the 2nd movie with a new one.
+   * Add one more movie at the end.
+   * Remove the 4th movie.
+   * Print the list and its length.
