b2ba0a92bdc7e98624791aa405afb9eb21668c8f
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-05-30 15:11:00 +0530

    Update Repeating_row.py

--- StarPatterns/Number_Pattern/Repeating_row.py
+++ StarPatterns/Number_Pattern/Repeating_row.py
@@ -1,1 +1,13 @@
-
+2. program repeating row number pattern 
+n = 5
+for I in range(n+1):
+    for j in range(1,I+1):
+        print(j,end=" ")
+    print()
+    
+Output:
+1 
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
