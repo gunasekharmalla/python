a1a9030a06e334e03b8820d018d3f5e131503bb7
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-05-30 15:03:03 +0530

    Update Increasing_Tri.py

--- StarPatterns/Number_Pattern/Increasing_Tri.py
+++ StarPatterns/Number_Pattern/Increasing_Tri.py
@@ -1,1 +1,13 @@
+1. simple increasing Triangle pattern 
+n = 5
+for I in range(n+1):
+    for j in range(1,I+1):
+        print(j,end=" ")
+    print()
 
+Output: 
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
