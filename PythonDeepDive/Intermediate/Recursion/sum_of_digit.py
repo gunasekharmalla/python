d5313e33bb4c033a8d853c9faf601541be499e3e
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-22 09:49:03 +0530

    Update sum_of_digit.py

--- PythonDeepDive/Intermediate/Recursion/sum_of_digit.py
+++ PythonDeepDive/Intermediate/Recursion/sum_of_digit.py
@@ -1,1 +1,17 @@
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    return n % 10 + sum_of_digits(n // 10)
 
+print(sum_of_digits(1234))  
+Output: 10
+-------------------------------------------------
+def digit(n, sum_):
+    if n == 0:
+        return sum_
+    sum_ += n % 10
+    return digit(n // 10, sum_)
+result = digit(1234, 0)
+print(result)   
+✅ Output: 10
+------------------------------------------
