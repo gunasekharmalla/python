55fb9c3d182aa6d4dbe3c4c9a289c8c771bc8db1
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-22 09:41:24 +0530

    Update math_examples.py

--- PythonDeepDive/Intermediate/modules_libraries/math_examples.py
+++ PythonDeepDive/Intermediate/modules_libraries/math_examples.py
@@ -1,1 +1,16 @@
+import math
+
+print("Ceil of 4.1:", math.ceil(4.1))
+print("Floor of 4.9:", math.floor(4.9))
+print("Square root of 49:", math.sqrt(49))
+print("Factorial of 5:", math.factorial(5))
+print("Sin(90°):", math.sin(math.radians(90)))
+
+Output:
+        Ceil of 4.1: 5
+        Floor of 4.9: 4
+        Square root of 49: 7.0
+        Factorial of 5: 120
+        Sin(90°): 1.0
+------------------------------------------------------------------------
 
