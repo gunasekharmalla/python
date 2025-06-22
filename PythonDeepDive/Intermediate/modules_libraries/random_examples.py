ffc9711dbf21ef9c24a72ebb071e464aa9f85b73
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-22 09:42:41 +0530

    Update random_examples.py

--- PythonDeepDive/Intermediate/modules_libraries/random_examples.py
+++ PythonDeepDive/Intermediate/modules_libraries/random_examples.py
@@ -1,1 +1,12 @@
+import random
 
+nums = [10, 20, 30, 40, 50]
+print("Random int:", random.randint(1, 100))
+print("Random choice from list:", random.choice(nums))
+print("Shuffled list:", random.sample(nums, len(nums)))
+
+Output:
+        Random int: 71
+        Random choice from list: 10
+        Shuffled list: [50, 20, 30, 40, 10]
+-------------------------------------------------------------------------------------
