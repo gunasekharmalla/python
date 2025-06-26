742c747544f1fbe0638a0bd7835b5986459bdb33
Malla Guna Sekhar <164771641+gunasekharmalla@users.noreply.github.com>
2025-06-26 21:53:56 +0530

    Update sum_of_N_num.py

--- PythonDeepDive/Intermediate/Recursion/sum_of_N_num.py
+++ PythonDeepDive/Intermediate/Recursion/sum_of_N_num.py
@@ -1,1 +1,26 @@
+def sum_n(n):
+    if n == 0:
+        return 0
+    return n + sum_n(n - 1)
 
+print(sum_n(4))
+
+call stack trace:
+
+sum_n(4)
+→ 4 + sum_n(3)
+
+sum_n(3)
+→ 3 + sum_n(2)
+
+sum_n(2)
+→ 2 + sum_n(1)
+
+sum_n(1)
+→ 1 + sum_n(0)
+
+sum_n(0)
+→ returns 0
+
+output:
+10
